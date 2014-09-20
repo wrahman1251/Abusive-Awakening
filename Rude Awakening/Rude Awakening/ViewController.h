445b03bd954566b3f2393ac206eace8ca9808f1c
Eric Webster <eric.webster@metricwire.com>
2014-09-20 03:33:29 -0400

    first commit

---
+++ Rude Awakening/Rude Awakening/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rude Awakening
+//
+//  Created by Eric Webster on 2014-09-20.
+//  Copyright (c) 2014 WAGE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Rude Awakening/Rude Awakening/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Rude Awakening
+//
+//  Created by Eric Webster on 2014-09-20.
+//  Copyright (c) 2014 WAGE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
