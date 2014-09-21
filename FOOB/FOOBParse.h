96210a8551d353d5a1219f63ade1db326a5aaaaa
Eric Webster <eric.webster@metricwire.com>
2014-09-20 23:10:56 -0400

    added parse

---
+++ FOOB/FOOBParse.h
@@ -0,0 +1,13 @@
+//
+//  FOOBParse.h
+//  FOOB
+//
+//  Created by Eric Webster on 2014-09-20.
+//  Copyright (c) 2014 WAGE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FOOBParse : NSObject
+
+@end
---
+++ FOOB/FOOBParse.m
@@ -0,0 +1,13 @@
+//
+//  FOOBParse.m
+//  FOOB
+//
+//  Created by Eric Webster on 2014-09-20.
+//  Copyright (c) 2014 WAGE. All rights reserved.
+//
+
+#import "FOOBParse.h"
+
+@implementation FOOBParse
+
+@end
